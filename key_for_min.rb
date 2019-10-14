eeb03ff0cfcadde1b5553e5f367735e6ab6838cc
William Stevens <wstevens.hornet@gmail.com>
2019-10-14 00:06:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,7 +12,7 @@ def key_for_min_value(name_hash)
     min_val = arr[0]
     i = 0
     array.each do |value|
-      min_num = value if value < min_num
+      min_val = value if value < min_num
     end
     name_hash.each do |key, value|
       if min_val == value
