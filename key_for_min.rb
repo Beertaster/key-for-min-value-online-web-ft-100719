5491924047fa2ac91acf3deab9f1a6054c91cfe6
William Stevens <wstevens.hornet@gmail.com>
2019-10-14 00:09:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,16 +5,16 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0
     return nil
   else
-    array = []
+    arr = []
     name_hash.each do |key, value|
-      array << value
+      arr << value
     end
-    min_num = array[0]
+    min_num = arr[0]
     array.each do |value|
-      min_num = value if value < min_num
+      min_val = value if value < min_val
     end
     name_hash.each do |key, value|
-      if min_num == value
+      if min_val == value
         return key
       end
     end
